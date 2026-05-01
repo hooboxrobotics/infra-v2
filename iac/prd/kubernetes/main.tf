7b90dcfa5f24df65894b78cc478d57561c920b66
Thiago Henrique <thiagohdeplima@gmail.com>
2026-05-01 09:01:38 -0300

    feat: create kubernetes for prd

---
+++ iac/prd/kubernetes/main.tf
@@ -0,0 +1,44 @@
+locals {
+  env  = "prd"
+  name = "hbx-${local.env}"
+
+  vpc_id = "vpc-0f606b0adf0e08852"
+  subnet_ids = [
+    "subnet-011901ea6568eb8b8",
+    "subnet-04f87bc83991303a7",
+    "subnet-06bbda0f2405ab4af"
+  ]
+
+  kubernetes_version = "1.34"
+}
+
+import {
+  to = module.eks.aws_eks_cluster.this[0]
+  id = local.name
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name       = local.name
+  vpc_id     = local.vpc_id
+  subnet_ids = local.subnet_ids
+
+  kubernetes_version = local.kubernetes_version
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = false
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  authentication_mode = "API"
+
+  tags = {
+    Env = local.env
+  }
+}
