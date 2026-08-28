fa1280af5a4498a29ca7131270e46376dd0b1d59
Thiago Henrique <thiagohdeplima@gmail.com>
2026-08-28 14:08:44 +0000

    feat(iac): provision redis

--- iac/hiae/prd/redis/main.tf
+++ iac/hiae/prd/redis/main.tf
@@ -22,15 +22,19 @@ resource "aws_security_group" "database" {
 module "elasticache" {
   source = "terraform-aws-modules/elasticache/aws"
 
-  cluster_id               = local.name
-  create_cluster           = true
-  create_replication_group = false
+  replication_group_id     = local.name
+  create_cluster           = false
+  create_replication_group = true
 
-  engine          = local.db_engine
-  engine_version  = local.db_version
-  node_type       = "cache.t4g.small"
-  num_cache_nodes = 2
-  az_mode         = "cross-az"
+  engine                     = local.db_engine
+  engine_version             = local.db_version
+  node_type                  = "cache.t4g.small"
+  num_cache_clusters         = 2
+  automatic_failover_enabled = true
+  multi_az_enabled           = true
+
+  transit_encryption_enabled = true
+  transit_encryption_mode    = "preferred"
 
   maintenance_window = "sun:05:00-sun:09:00"
   apply_immediately  = true
@@ -39,4 +43,22 @@ module "elasticache" {
 
   security_group_ids = [aws_security_group.database.id]
   subnet_ids         = data.aws_subnets.database.ids
+}
+
+resource "aws_ssm_parameter" "redis_host" {
+  name  = "/kubernetes/hbx-prd/np-api-gateway/REDIS_HOST"
+  type  = "String"
+  value = module.elasticache.replication_group_primary_endpoint_address
+}
+
+resource "aws_ssm_parameter" "redis_port" {
+  name  = "/kubernetes/hbx-prd/np-api-gateway/REDIS_PORT"
+  type  = "String"
+  value = tostring(module.elasticache.replication_group_port)
+}
+
+resource "aws_ssm_parameter" "redis_tls" {
+  name  = "/kubernetes/hbx-prd/np-api-gateway/REDIS_TLS"
+  type  = "String"
+  value = "true"
 }
